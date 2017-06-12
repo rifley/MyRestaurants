80ad124078c4f5886378e83fa6fca17536ecf8a0
mark fisher and riley watts <mark@>
2017-06-12 11:26:24 -0700

    implement animations with java

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantsViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantsViewHolder.java
@@ -3,6 +3,7 @@ package com.epicodus.myrestaurants.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
+import com.epicodus.myrestaurants.util.ItemTouchHelperViewHolder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,7 +30,7 @@ import java.util.ArrayList;
  * Created by Guest on 6/5/17.
  */
 
-public class FirebaseRestaurantsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class FirebaseRestaurantsViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder{
     private static final int MAX_WIDTH =  200;
     private static final int MAX_HEIGHT = 200;
     public ImageView mRestaurantImageView;
@@ -57,7 +59,7 @@ public class FirebaseRestaurantsViewHolder extends RecyclerView.ViewHolder imple
         categoryTextView.setText(restaurant.getCategories().get(0));
         ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
     }
-    @Override
+
     public void onClick(View view) {
         final ArrayList<Restaurant> restaurants = new ArrayList<>();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -88,4 +90,20 @@ public class FirebaseRestaurantsViewHolder extends RecyclerView.ViewHolder imple
         });
     }
 
+    @Override
+    public void onItemSelected() {
+        itemView.animate()
+                .alpha(0.7f)
+                .scaleX(0.9f)
+                .scaleY(0.9f)
+                .setDuration(500);
+    }
+
+    @Override
+    public void onItemClear() {
+        itemView.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f);
+    }
 }
